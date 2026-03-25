99db1db99411730721186bcf71334a8209381962
Brandon Chin <brandonhchin@gmail.com>
2026-03-24 19:14:10 -0700

    Decided on a design change -> classes representing each sequence that takes two attributes: header, sequence.

--- file_structure.py
+++ file_structure.py
@@ -1,4 +1,22 @@
-class fasta_structure:
-    def __init__(self, filename):
-        self.filename = filename
+class Sequence:
+    def __init__(self, header, sequence):
+        self._header = header[1:]
+        self._sequence = sequence
+
+    def sequence(self):
+        return self._sequence
+
+    def header(self):
+        return self._header
+
+    def gc_content(self):
+        '''returns amount of G and C in a sequence'''
+        count = 0
+        for x in self._sequence:
+            if x == 'C' or x == 'G':
+                count +=1
+
+        return count/2
+
+
 
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from parser import parse_fasta
 
 def main():
 
+    #idea 1
+    #create class representing each individual sequence
+    #which will have callable methods such as GC content
+
+
+
     sequences = parse_fasta('mus-musculus-hemoglobin.fasta')
     print(sequences)
 
